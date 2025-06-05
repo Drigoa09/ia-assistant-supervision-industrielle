978ddbcf639c33ae4a160a6682d6257d4bafdcef
Benjelloun Harzimi Saad <saadbenjelloun05@gmail.com>
2025-06-05 13:24:39 +0200

    Database

--- Agents/Tools_nodes/database_node.py
+++ Agents/Tools_nodes/database_node.py
@@ -3,12 +3,28 @@ from model import model
 
 from langchain_core.tools import tool
 
-@tool
-def get_element(a, b) -> int:
-    '''Multiply a and b'''
-    return a + b
+from elasticsearch import Elasticsearch
+import os
 
-tools = [get_element]
+es = Elasticsearch(
+    os.getenv("ES_HOST"),
+    basic_auth=(os.getenv("ES_USER"), os.getenv("ES_PASSWORD")),
+)
+
+@tool
+def search_elasticsearch(query: dict) -> list:
+    """
+    Reçoit une requête Elasticsearch (au format dict) et retourne les documents.
+    """
+    response = es.search(
+        index=os.getenv("ES_INDEX"),
+        query=query,
+        size=10
+    )
+    return [hit["_source"] for hit in response["hits"]["hits"]]
+
+tools = [search_elasticsearch]
 tool_node = ToolNode(tools)
 
+# LLM lié aux outils
 llm_with_tools = model.bind_tools(tools)
